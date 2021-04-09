42130ab450052069578e7d3bd420940e37ce7178
sk2111 <sathisht1996@gmail.com>
2021-04-09 12:42:47 +0530

    Add : Reducer setup
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //libs
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'redux';
+import { Provider } from 'react-redux';
 import { store } from "./redux/store";
 import App from './pages/App/App.component';
 
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,7 +7,7 @@ import { combineReducers } from 'redux';
 
 
 const appReducer = combineReducers({
-    todolist: {}
+    todolist: () => ({ dummy: 1 })
 });
 
 
